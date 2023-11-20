43b8992efb3f72e7e31282475e255227cfe93167
YevheniiKost <Atom903@gmail.com>
2023-11-20 22:19:31 +0200

    Add Player Movement Module

--- Source/BoomInternal.Target.cs
+++ Source/BoomInternal.Target.cs
@@ -11,5 +11,11 @@ public class BoomInternalTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("BoomInternal");
+		RegisterModulesCreatedByRider();
+	}
+
+	private void RegisterModulesCreatedByRider()
+	{
+		ExtraModuleNames.AddRange(new string[] { "PlayerMovement" });
 	}
 }
---
+++ Source/PlayerMovement/Private/PlayerMovement.cpp
@@ -0,0 +1,17 @@
+﻿#include "PlayerMovement.h"
+
+#define LOCTEXT_NAMESPACE "FPlayerMovementModule"
+
+void FPlayerMovementModule::StartupModule()
+{
+    
+}
+
+void FPlayerMovementModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FPlayerMovementModule, PlayerMovement)
